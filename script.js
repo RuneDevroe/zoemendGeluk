728901f2927f9e1619617241e17429d7eec38883
LAPTOP-FH69HDVC\Rune <rune.devroe@outlook.be>
2025-01-26 15:24:07 +0100

    slides2.0

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) { slideIndex = 1; }
   slides[slideIndex - 1].style.display = "block";
-  slideTimer = setTimeout(showSlides, 2000); // Change image every 2 seconds
+  slideTimer = setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
 
 function plusSlides(n) {
